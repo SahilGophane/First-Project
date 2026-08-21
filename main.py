011daae4f6798a836bdd1c5b26c9cc07425971d1
Sahil Gophane <sahilgophane@gmail.com>
2026-08-21 16:03:57 +0530

    making changes in longest one

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-class Solution:
-    def longestCommonPrefix(self, str):
-        return str
-f = 'this is something'
-d = Solution()
-print(d.longestCommonPrefix(f))
+strs = ["flower", "flow", "flight"]
+my_dict = {index:value for index, value in enumerate(strs)}
+#print(my_dict)
+for i, value in my_dict.items():
+    for j in range(i + 1, len(my_dict)):
+        if value.startswith(my_dict[j]):
+            print(f"{value} starts with {my_dict[j]}")
+        elif my_dict[j].startswith(value):
+            print(f"{my_dict[j]} starts with {value}")
